71f05d088352802f1cfba90d9b45df753033e44e
savannahgriffith <savannah.griffith@jpschools.org>
2026-06-23 15:59:35 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -5,7 +5,7 @@ var init = function (window) {
     app = window.opspark.makeApp(),
     canvas = app.canvas,
     view = app.view,
-    fps = draw.fps("#000");
+    fps = draw.fps("#d05d5d");
 
   window.opspark.makeGame = function () {
     window.opspark.game = {};
@@ -21,20 +21,18 @@ var init = function (window) {
 
     // TODO 2 : Create a function that draws a circle
     function drawCircle() {
-      circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+      circle = draw.randomCircleInArea(canvas, true, true, "#e66ac1aa", 2);
       physikz.addRandomVelocity(circle, canvas, 5, 5);
       view.addChild(circle);
       circles.push(circle);
     }
 
     // TODO 3 : Call the drawCircle() function
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
-    drawCircle();
 
     // TODO 7 : Use a loop to create multiple circles
+    for (var i = 0; i < 50; i++) {
+      drawCircle();
+    }
 
     ///////////////////
     // PROGRAM LOGIC //
@@ -47,20 +45,17 @@ var init = function (window) {
         */
     function update() {
       // TODO 4 : Update the position of each circle using physikz.updatePosition()
-      physikz.updatePosition(circles[0]);
-      physikz.updatePosition(circles[1]);
-      physikz.updatePosition(circles[2]);
-      physikz.updatePosition(circles[3]);
-      physikz.updatePosition(circles[4]);
+     
 
       // TODO 5 : Call game.checkCirclePosition() on your circles
-      game.checkCirclePosition(circles[0]);
-      game.checkCirclePosition(circles[1]);
-      game.checkCirclePosition(circles[2]);
-      game.checkCirclePosition(circles[3]);
-      game.checkCirclePosition(circles[4]);
+    
 
       // TODO 8 / TODO 9 : Iterate over the array
+      for (var i = 0; i < circles.length; i++) {
+        // Code to update each circle
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
     }
 
     /* 
@@ -75,23 +70,21 @@ var init = function (window) {
       }
 
       // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-      if (circle.x < 0 - circle.radius) {
-        circle.x = canvas.width + circle.radius;
+      if (circle.x < 0 ) {
+        circle.x = canvas.width ;
       }
 
-      // Right Boundary (Assuming canvas coordinates)
-      if (circle.x > canvas.width + circle.radius) {
-        circle.x = 0 - circle.radius;
-      }
+    
+      
 
       // Top Boundary
-      if (circle.y < 0 - circle.radius) {
-        circle.y = canvas.height + circle.radius;
+      if (circle.y < 0) {
+        circle.y = canvas.height;
       }
 
       // Bottom Boundary
-      if (circle.y > canvas.height + circle.radius) {
-        circle.y = 0 - circle.radius;
+      if (circle.y > canvas.height) {
+        circle.y = 0 ;
       }
 
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
